a18a173dcee0f7b7fc21b1da07b315207c2d8159
Amrit Lahari <f20230442@pilani.bits-pilani.ac.in>
2025-11-29 19:54:08 +0530

    all errors fixed

--- check_plots.py
+++
@@ -1,7 +0,0 @@
-with open('DRM_Project.ipynb', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Check for savefig before plt.show()
-for i in [333, 700, 1618, 1917, 2149, 2685]:
-    print(f"\n=== Line {i+1} ===")
-    print(''.join(lines[max(0,i-2):i+3]))
